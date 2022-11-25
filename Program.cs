8288e913e689c31dedf4ac89c2ec0a4201f3b487
Abdul Eriawan <abdul.eriawan.ft18@mail.umy.ac.id>
2022-11-25 16:03:27 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise3_2018118
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
