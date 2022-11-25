a27e959f7d83b7aadcd6f05c168931086065df18
Abdul Eriawan <abdul.eriawan.ft18@mail.umy.ac.id>
2022-11-25 16:17:53 +0700

    traverse done

--- Program.cs
+++ Program.cs
@@ -45,6 +45,26 @@ namespace Exercise3_2018118
                 else
                     return false;
             }
+
+            public void traverse()/*traverses all the nodes of the list*/
+            {
+                if (listEmpty())
+                    Console.WriteLine("\nList is empty");
+                else
+                {
+                    Console.WriteLine("\nRecords in the list are:\n");
+                    Node currentNode;
+                    currentNode = LAST.next;
+                    while (currentNode != LAST)
+                    {
+                        Console.Write(currentNode.rollNumber + "     " +
+                            currentNode.name + "\n");
+                        currentNode = currentNode.next;
+                    }
+                    Console.Write(LAST.rollNumber + "      " + LAST.name + "\n");
+                }
+            }
+                
         }
         
         static void Main(string[] args)
